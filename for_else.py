5f87f580552ab3d3253e3e04367333ac5dd29b04
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-04 12:29:07 -0300

    Add files via upload

---
+++ for_else.py
@@ -0,0 +1,15 @@
+"""
+
+For/Else
+
+
+"""
+
+var = ["Luiz", "Joanna", "Maria", "Laura"]
+
+for valor in var:
+    if valor.startswith("L"):
+        print("O nome", valor, "começa com L")
+    else:
+        print("O nome", valor, "não começa com L")
+
